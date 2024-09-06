a6f37d04ced6500be4bac67816a75c5002af1850
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-06 21:02:28 +0100

    commit

--- src/counter-app/manageCounter.jsx
+++ src/counter-app/manageCounter.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import useCounter from '../store/useCounterStore'
 
 export const ManageCounter = () => {
+    const incrementCount = useCounter((state) => state.handleIncrementCount);
+
     return (
         <div className=' flex justify-center'>
-            <button className=' mb-5 bg-black text-white text-lg font-extrabold py-1 px-6 rounded-xl'>
+            <button onClick={incrementCount} className=' mb-5 bg-black text-white text-lg font-extrabold py-1 px-6 rounded-xl'>
                 Handle Counter Value
             </button>
         </div>
--- src/store/useCounterStore.jsx
+++ src/store/useCounterStore.jsx
@@ -3,8 +3,10 @@ import { create } from "zustand";
 const useCounter = create((set) => {
     return {
         count : 5000,
-        name : "Marvel Ayomike",
-    };
+        handleIncrementCount: () => set((state) => ({
+            count: state.count + 1,
+        }))
+    }   
 });
 
 export default useCounter;
