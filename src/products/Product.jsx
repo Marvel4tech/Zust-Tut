2865d5c1c4efad104f215ea7440b4f5a08ec954d
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-06 22:33:00 +0100

    commit

--- src/products/Product.jsx
+++ src/products/Product.jsx
@@ -12,16 +12,18 @@ const Product = () => {
   return (
     <div className=' text-center font-medium mt-10'>
         <h1 className=' font-semibold'>List of Products</h1>
-        {
-            productLists?.length > 0 ? (
-                productLists.map((product, i) => {
-                    <div key={i}>
-                        <p>{product?.title}</p>
-                        <img src={product?.thumbnail} alt="" />
-                    </div>
-                })
-            )
-        }
+       <ul>
+            {
+                productLists?.length > 0 ? (
+                    productLists.map((product, i) => {
+                        <div key={i}>
+                            <p>{product?.title}</p>
+                            <img src={product?.thumbnail} alt="" />
+                        </div>
+                    })
+                ) : <p>No Products Listed</p>
+            }
+       </ul>
     </div>
   )
 }
