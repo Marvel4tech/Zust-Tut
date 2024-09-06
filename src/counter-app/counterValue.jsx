9baf4351852201f484d2490b76d629c287264560
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-06 20:08:48 +0100

    commit

--- src/counter-app/counterValue.jsx
+++ src/counter-app/counterValue.jsx
@@ -1,13 +1,13 @@
 import useCounter from "../store/useCounterStore"
 
 export const CounterValue = () => {
-    const stateValue = useCounter(state => state)
+    const stateValue = useCounter((state) => state.count)
 
     console.log(stateValue)
 
     return (
         <div className=" text-center font-medium">
-            Counter Value is 100
+            {stateValue}
         </div>
     )
 }
