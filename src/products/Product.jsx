08f3eacb4fcd2e3ff14da760f04b3a360c295b55
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-06 22:29:48 +0100

    commit

--- src/products/Product.jsx
+++ src/products/Product.jsx
@@ -3,15 +3,25 @@ import useCounter from '../store/useCounterStore'
 
 const Product = () => {
     const fetchProducts = useCounter((state) => state.fetchListOfProducts)
+    const productLists = useCounter((state) => state.listOfProducts)
 
     useEffect(() => {
-
+        fetchProducts()
     }, [])
 
   return (
-    <div>
-        <h1>List of Products</h1>
-
+    <div className=' text-center font-medium mt-10'>
+        <h1 className=' font-semibold'>List of Products</h1>
+        {
+            productLists?.length > 0 ? (
+                productLists.map((product, i) => {
+                    <div key={i}>
+                        <p>{product?.title}</p>
+                        <img src={product?.thumbnail} alt="" />
+                    </div>
+                })
+            )
+        }
     </div>
   )
 }
